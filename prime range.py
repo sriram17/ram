5030453a11dad01a818f8e2e873f4357723a4dd3
sriram17 <46023056+sriram17@users.noreply.github.com>
2019-05-27 23:29:11 +0530

    Update prime range.py

--- prime range.py
+++ prime range.py
@@ -1,6 +1,12 @@
 a,b=map(int,input().split())
 c=[2,3,5,7,11,13]
+k=[]
 for i in range(a+1,b):
   if (i not in c):
-    if(())
+    if((i%2!=0)and(i%3!=0)and(i%5!=0)and(i%7!=0)and(i%11!=0)and(i%13!=0)):
+      k.append(i)
+  else:
+    k.append(i)
+print(*k)
+      
   
