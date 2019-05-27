abb41e16164960a2dcb0fc4ec632bb50b487b304
sriram17 <46023056+sriram17@users.noreply.github.com>
2019-05-27 23:16:12 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,5 +1,9 @@
 a=int(input())
-if((a%2!=0)and(a%3!=0)and(a%5!=0)and(a%7!=0)and(a%11!=0)and(a%13!=0)):
-  print("yes")
+b=[2,3,5,7,11]
+if (a not in b):
+  if((a%2!=0)and(a%3!=0)and(a%5!=0)and(a%7!=0)and(a%11!=0)and(a%13!=0)):
+    print("yes")
+  else:
+    print("no")
 else:
-  print("no")
+  print("yes")
