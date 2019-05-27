ad143cb02c31ef2be49f726d5ac149ccdd76a941
sriram17 <46023056+sriram17@users.noreply.github.com>
2019-05-27 23:50:48 +0530

    Update armstrong number range.py

--- armstrong number range.py
+++ armstrong number range.py
@@ -1,9 +1,9 @@
 a,b=input().split()
 c=0
 l=[]
-for i in range(int(a+1),int(b)):
+for i in range(int(a)+1,int(b)):
   for j in str(i):
-    c=c+int(j)**len(i)
+    c=c+int(j)**len(str(i))
   if(i==c):
     l.apprnd(i)
   c=0
