90dfce71e3cf2fd4c4bfa3f9fb587cd331bf615f
sriram17 <46023056+sriram17@users.noreply.github.com>
2019-05-27 23:47:38 +0530

    Update armstrong number range.py

--- armstrong number range.py
+++ armstrong number range.py
@@ -1,3 +1,10 @@
-a,b=map(int,input().split())
-for i in range(a+1,b):
-  
+a,b=input().split()
+c=0
+l=[]
+for i in range(int(a+1),int(b)):
+  for j in str(i):
+    c=c+int(j)**len(i)
+  if(i==c):
+    l.apprnd(i)
+  c=0
+print(*l)
