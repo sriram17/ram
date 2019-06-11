66534f9cac3c2668940fbfe7f00dd576d5b78eb4
sriram17 <46023056+sriram17@users.noreply.github.com>
2019-06-11 13:28:49 +0530

    Update fibb.py

--- fibb.py
+++ fibb.py
@@ -2,7 +2,9 @@ N=int(input())
 a=list(map(int,input().split()))
 b=0
 c=0
-for i in a:
-  b=i-b
-  c=c+b
+for i in range(0,len(a)):
+  if(i==0):
+    c=a[i]-c
+  else:
+    c=c+(a[i]-a[0])
 print(c)
