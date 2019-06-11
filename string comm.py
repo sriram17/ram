ec186a906243d64a37ee41639610ade9993821b6
sriram17 <46023056+sriram17@users.noreply.github.com>
2019-06-11 12:43:14 +0530

    Update string comm.py

--- string comm.py
+++ string comm.py
@@ -1,11 +1,25 @@
 N=int(input())
 a=input()
 k=""
+b=[]
 for i in range(0,N-1):
   s=input()
-  for j in range(0,len(a)):
-    if(a[j] in s ):
-      k=k+a[j]
-      a=k
+  for j in range(1,len(a)):
+    if(a[0:j] in s ):
+      k=a[0:j]
+  b.append(k)
   k=""
-print(k)
+m=b
+count=0
+for g in b:
+    m.remove(g)
+    for l in m:
+        if(g in l):
+            count=count+1
+    if(count>=N//2):
+        ans=g
+    m=b
+    count=0
+print(ans)
+    
+    
