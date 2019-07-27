eb0c08fd792649bbc85ecafe002d4b6a5b9f8636
sriram17 <46023056+sriram17@users.noreply.github.com>
2019-07-27 20:16:12 +0530

    Update maxskip.py

--- maxskip.py
+++ maxskip.py
@@ -2,7 +2,10 @@ n=int(input())
 a=list(map(int,input().split()))
 c=[]
 pro=1
-for i in a:
-    pro=pro*i
-    c.append(pro)
+for i in range(n):
+    b=a[i:]
+    for j in b:
+        pro=pro*j
+        c.append(pro)
+    pro=1
 print(max(c))
